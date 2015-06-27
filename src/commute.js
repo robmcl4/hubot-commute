0b7ac5ee9770a1dabdf84cbdc17dedba5f6ea47c
Robert McLaughlin <robert@sparkk.us>
2015-06-26 22:20:30 -0400

    migrate to mapquest api

--- src/commute.js
+++ src/commute.js
@@ -2,43 +2,48 @@
 //   A hubot script for the Crescent Commute
 //
 // Configuration:
-//   HUBOT_COMMUTE_MAPS_KEY: your google maps API key
-//   HUBOT_COMMUTE_MAPS_WORK: your work address
-//   HUBOT_COMMUTE_MAPS_HOME: your home address
+//   HUBOT_COMMUTE_MAPS_KEY: your mapquest API key
+//   HUBOT_COMMUTE_MAPS_WORK: your work address as a Location
+//   HUBOT_COMMUTE_MAPS_HOME: your home address as a Location
+//   Locations are formatted as JSON strings:
+//     {
+//       "street":     "1 Happy Street",
+//       "city":       "Springfield",
+//       "state":      "NY",
+//       "postalCode": "12345"
+//     }
 //
 // Commands:
-//   hubot hello - <what the respond trigger does>
-//   orly - <what the hear trigger does>
 //
 // Notes:
 //   <optional notes required for the script>
 //
 // Author:
 //   Robert McLaughlin <rmclaughlin@constantcontact.com>
-var GoogleMapsAPI = require('googlemaps');
 
-var gm = new GoogleMapsAPI({
-  key: process.env['HUBOT_COMMUTE_MAPS_KEY'],
-  stagger_time: 1000,
-  encode_polylines: false,
-  secure: true
-});
-
-var query = {
-  origin: process.env['HUBOT_COMMUTE_MAPS_WORK'],
-  destination: process.env['HUBOT_COMMUTE_MAPS_HOME']
-};
+var baseUrl = 'http://www.mapquestapi.com',
+    basePath = '/directions/v2/route',
+    key = process.env['HUBOT_COMMUTE_MAPS_KEY'],
+    work = JSON.parse(process.env['HUBOT_COMMUTE_MAPS_WORK']),
+    home = JSON.parse(process.env['HUBOT_COMMUTE_MAPS_HOME']);
 
 module.exports = function (robot) {
+  var api = robot.http(baseUrl).path(basePath);
+
   robot.respond(/maps\sdebug/, function(res) {
     console.log('beginning map debug');
-    gm.directions(query, function(err, result) {
-      if (err) {
-        res.reply('Oops! I can\'t do that!');
-        console.error(err.stack);
-      } else {
-        res.reply('/code\n'+JSON.stringify(result));
-      }
+    var data = JSON.stringify({
+      locations: [work, home]
     });
+    console.log(data);
+    api.header('Accept', 'application/json')
+       .query({key: key})
+       .post(data)(function(err, response, body) {
+         if (err) {
+           console.error(err.stack);
+           return res.reply('Oops!');
+         }
+         res.reply(body);
+       });
   });
 }
